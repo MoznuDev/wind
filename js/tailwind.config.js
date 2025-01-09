836cb057ab3e8d1764fe91965447c9f03ecd6f42
Moznur Rahman <rmoznu82@gmail.com>
2025-01-09 22:49:38 +0600

    Add files via upload

---
+++ js/tailwind.config.js
@@ -0,0 +1,41 @@
+
+tailwind.config = {
+  theme: {
+    extend: {
+      // colors 
+      colors: {
+        headerBg: '#DBD5FB',
+        btnColor:'#8773EF',
+        bgColor:'#19153D',
+      },
+      // Color 
+      // Container 
+      maxWidth: {
+        'headerContainer': '1170px',
+      },
+      // Container 
+      // Font 
+      fontFamily: {
+        'open': ["Open Sans"],
+        'paprika': ["Paprika"],
+        'roboto': ["Roboto"],
+        'nunito': ["Nunito Sans"],
+      },
+      // Font
+      // background 
+      backgroundImage: {
+        bannerBg: "url('./images/bannerImg.png')",
+        salesBg: "url('./images/helf.png')",
+        storyBg: "url('./images/story2.png')",
+        businessBg: "url('./images/businessBg.png')",
+        chooseBg: "url('./images/chooseBg.png')",
+        thinkBg: "url('./images/thinkBg.png')",
+        chooseBg: "url('./images/chooseBg.png')",
+        selectedBg: "url('./images/selectedBg.png')",
+        thinkBg: "url('./images/thinkBg.png')", 
+        
+      },
+      // Background 
+      }
+    }
+  }
